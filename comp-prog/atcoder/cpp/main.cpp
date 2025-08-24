5673e3b852c645a2177d70a28ce4dfe283137b42
asana17 <kei17.kaight084@gmail.com>
2025-08-24 22:56:13 +0900

    Remove unnecessary function in atcoder main.cpp
    Signed-off-by: asana17 <kei17.kaight084@gmail.com>

--- comp-prog/atcoder/cpp/main.cpp
+++ comp-prog/atcoder/cpp/main.cpp
@@ -1,23 +1,13 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int N;
+//int N;
 
 using ll = long long;
 
-template <class T>
-void debug_vector(vector<T> v)
-{
-  int index = 0;
-  for (auto c : v) {
-    cout << "index " << index << ": " << c << endl;
-    index++;
-  }
-}
-
 int main(int argc, char *argv[])
 {
-  cin >> N;
+  //cin >> N;
 
   return 0;
 }
